4bce99bb8b2e13e29cef6bc6937c78141ace420a
StefanB272 <stefan.bodur@gmail.com>
2024-01-05 17:29:35 +0200

    proiecte

---
+++ exdart/liste/3/2.amm.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main() {
+  int n = int.parse(stdin.readLineSync()!);
+  int min = 999999;
+  int max = -999999, pozmax = 0, pozmin = 0;
+  List<int> nr = [];
+  for (int i = 0; i < n; i++) {
+    int x = int.parse(stdin.readLineSync()!);
+    nr.add(x);
+    if (x < min) {
+      min = x;
+      pozmin = i;
+    }
+    if (x > max) {
+      max = x;
+      pozmax = i;
+    }
+  }
+  if (pozmin > pozmax) {
+    int h = 0;
+    int j = pozmin;
+    int g = pozmax;
+    h = pozmin;
+    pozmin = pozmax;
+    pozmax = h;
+  }
+  for (int z = pozmin; z <= pozmax; z++) {
+    print(nr[z]);
+  }
+}
