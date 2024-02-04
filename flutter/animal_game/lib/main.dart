e3a998d2470203a3e732c7fb537742663e99291f
StefanB272 <stefan.bodur@gmail.com>
2024-02-04 12:12:32 +0200

    hdsa

--- flutter/animal_game/lib/main.dart
+++ flutter/animal_game/lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
         //   animalsList: animals,
         // ),
         home: QuestionScreen(
-          animalProperty: AnimalProperty.fourLegs,
+          animalProperty: AnimalProperty.carnivore,
         ));
   }
 }
--- flutter/animal_game/lib/screen/question_screen.dart
+++ flutter/animal_game/lib/screen/question_screen.dart
@@ -7,6 +7,38 @@ class QuestionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          animalProperty.question,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontStyle: FontStyle.italic, fontSize: 32), //Shadows Into Light
+        ),
+        SizedBox(height: 10),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {},
+                child: Text("YES!",
+                    style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        fontSize: 36))), //Rubik Scribble
+            SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {},
+                child: Text("NO!",
+                    style:
+                        TextStyle(fontStyle: FontStyle.italic, fontSize: 36))),
+          ],
+        )
+      ],
+    ));
+
     return Center(child: Text(animalProperty.question));
   }
 }
